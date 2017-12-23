24168bbb0eb2d2d6da12647fc2274972d07dba00
Ozan Dost ŞAHİN <odost.sahin@tedu.edu.tr>
2017-12-24 00:13:02 +0300

    INIT

---
+++ ManCave/ManCave/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManCave.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ ManCave/ManCave/Models/User/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManCave.Models.User
+{
+    public class User
+    {
+        public bool isAdmin { get; set};
+        string username;
+
+    }
+}
